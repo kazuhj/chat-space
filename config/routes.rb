5c9782d72d17f90aa2196c54d5a03baa453ce07c
kazuya hojo <maboai.kazu29@gmail.com>
2019-11-19 23:54:23 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index]
+    resources :chats, only: [:index, :create]
   end
 end
