8b0ac0170bb323985a13402468760d50d7623183
kazuya hojo <maboai.kazu29@gmail.com>
2019-11-19 23:10:37 +0900

    chatモデル作成

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,5 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
   # 自身のDB設計にはないが、回答に基きバリデーションを追加。
   validates :name, presence: true, uniqueness: true
+  has_many :chats
 end
