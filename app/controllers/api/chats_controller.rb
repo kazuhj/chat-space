592d92c54d17175478c5ed220516154050661387
kazuya hojo <maboai.kazu29@gmail.com>
2019-12-12 18:50:41 +0900

    新規投稿を取得
    indexアクション定義
    ルーティング追加

---
+++ app/controllers/api/chats_controller.rb
@@ -0,0 +1,7 @@
+class Api::ChatsController < ApplicationController
+  def index
+    group = Group.find(params[:group_id])
+    last_message_id = params[:id].to_i
+    @messages = group.chats.includes(:user).where("id > #{last_messages_id}")
+  end
+end
