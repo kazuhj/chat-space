9982ede42e2eeb98276a1779cd17198c8de241ab
kazuya hojo <maboai.kazu29@gmail.com>
2019-12-12 20:35:11 +0900

    @messages→@chats

--- app/controllers/api/chats_controller.rb
+++ app/controllers/api/chats_controller.rb
@@ -2,6 +2,6 @@ class Api::ChatsController < ApplicationController
   def index
     group = Group.find(params[:group_id])
     last_message_id = params[:id].to_i
-    @messages = group.chats.includes(:user).where("id > #{last_messages_id}")
+    @chats = group.chats.includes(:user).where("id > #{last_messages_id}")
   end
 end
---
+++ app/views/api/chats/index.json.jbuilder
@@ -0,0 +1,7 @@
+json.array! @chats do |chat|
+  json.content chat.content
+  json.image chat.image.url
+  json.created_at chat.created_at.strftime("%Y年%m月%d日 %H時%M分")
+  json.user_name chat.user.user_name
+  json.id chat.id
+end
